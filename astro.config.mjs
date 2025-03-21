865c90dd831ac3a6fef1f424a7bc0b6e554b3da8
maui <hello@mauriciabad.com>
2025-03-22 00:24:17 +0200

    Add env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [db()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  env: {
+    schema: {
+      ASTRO_DB_REMOTE_URL: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      ASTRO_DB_APP_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
 });
