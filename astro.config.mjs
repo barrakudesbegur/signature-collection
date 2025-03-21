d24a8d3a47b9ca757f4636a3d7efda0a2186bbf7
maui <hello@mauriciabad.com>
2025-03-21 21:29:04 +0200

    Add database

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import db from '@astrojs/db';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [db()],
+  adapter: cloudflare()
 });
